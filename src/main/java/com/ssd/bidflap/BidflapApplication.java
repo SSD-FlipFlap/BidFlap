92691a80de57d1068d22c7882370acdfc0edaae6
이승연 <dltmddus9082@naver.com>
2024-05-01 23:17:50 +0900

    [ADD] 프로젝트 초기 세팅

---
+++ src/main/java/com/ssd/bidflap/BidflapApplication.java
@@ -0,0 +1,13 @@
+package com.ssd.bidflap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidflapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidflapApplication.class, args);
+	}
+
+}
