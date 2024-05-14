0f0755ed91006fff77383077db9ca328a2cbc4b7
이승연 <dltmddus9082@naver.com>
2024-05-15 02:38:02 +0900

    [REFACTOR] Category enum 변경

--- src/main/java/com/ssd/bidflap/domain/enums/Category.java
+++ src/main/java/com/ssd/bidflap/domain/enums/Category.java
@@ -1,23 +1,13 @@
 package com.ssd.bidflap.domain.enums;
 
 public enum Category {
-    MOBILE("mobile"),
-    TABLET("tablet"),
-    CAMERA("camera"),
-    PC_PARTS("pc_parts"),
-    WEARABLE("wearable"),
-    PC_NOTEBOOK("pc_notebook"),
-    ACCESSORY("accessory"),
-    ETC("etc"),
-    ALL("all");
-
-    private final String value;
-
-    Category(String value) {
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
+    MOBILE,
+    TABLET,
+    CAMERA,
+    PC_PARTS,
+    WEARABLE,
+    PC_NOTEBOOK,
+    ACCESSORY,
+    ETC,
+    ALL
 }
