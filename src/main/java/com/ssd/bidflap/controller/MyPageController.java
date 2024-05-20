6358aaba23bd44da9c4f14db3cc83a44143a5ff5
이승연 <dltmddus9082@naver.com>
2024-05-20 23:40:12 +0900

    [ADD] 마이페이지, 회원 정보 수정 페이지 경로 연결

--- src/main/java/com/ssd/bidflap/controller/MyPageController.java
+++ src/main/java/com/ssd/bidflap/controller/MyPageController.java
@@ -2,7 +2,11 @@ package com.ssd.bidflap.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/members")
@@ -13,4 +17,23 @@ public class MyPageController {
         return "thyme/member/myPage";
     }
 
+    @GetMapping("/edit-profile")
+    public String editProfile() {
+        return "thyme/member/editProfile";
+    }
+
+    // 뷰에 은행 목록을 전달(drop-down list)
+    @ModelAttribute("bankList")
+    public List<String> bankList() {
+        List<String> bankList = new ArrayList<>();
+        bankList.add("국민");
+        bankList.add("신한");
+        bankList.add("우리");
+        bankList.add("하나");
+        bankList.add("농협");
+        bankList.add("수협");
+        bankList.add("산업");
+        bankList.add("카카오뱅크");
+        return bankList;
+    }
 }
