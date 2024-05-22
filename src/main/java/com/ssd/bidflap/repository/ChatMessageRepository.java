223838c2e03d9d4c9b41b220ea5ae065930fe1f5
karam Choi <riverchoi2523@gmail.com>
2024-05-23 05:37:53 +0900

    [ADD] 채팅 기능 구현 완료

---
+++ src/main/java/com/ssd/bidflap/repository/ChatMessageRepository.java
@@ -0,0 +1,16 @@
+package com.ssd.bidflap.repository;
+
+import com.ssd.bidflap.domain.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    public List<ChatMessage> getChatMessagesByChatRoomId(long chatRoomId);
+
+    public ChatMessage save(ChatMessage chatMessage);
+
+    public void deleteByChatRoomId(int chatRoomId);
+}
