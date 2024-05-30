5e91918569861010b7794d6334fbe5269713668a
karam Choi <riverchoi2523@gmail.com>
2024-05-30 22:46:03 +0900

    [ADD]채팅방 생성 및 seller 정보 반영

--- src/main/java/com/ssd/bidflap/controller/ChatController.java
+++ src/main/java/com/ssd/bidflap/controller/ChatController.java
@@ -2,11 +2,10 @@ package com.ssd.bidflap.controller;
 
 import com.ssd.bidflap.domain.*;
 import com.ssd.bidflap.domain.dto.ChatMessageDto;
+import com.ssd.bidflap.repository.AfterServiceRepository;
 import com.ssd.bidflap.repository.MemberRepository;
 import com.ssd.bidflap.repository.ProductRepository;
 import com.ssd.bidflap.service.ChatService;
-import com.ssd.bidflap.service.MemberService;
-import com.ssd.bidflap.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.webjars.NotFoundException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +27,7 @@ public class ChatController {
     private final ChatService chatService;
     private final ProductRepository productRepository;
     private final MemberRepository memberRepository;
+    private final AfterServiceRepository asRepository;
 
     //채팅방리스트 - 마이페이지
     @GetMapping("/chatRooms")
@@ -53,7 +54,7 @@ public class ChatController {
         if(product == null)
             throw new NotFoundException("product를 찾을 수 없습니다.");
         modelAndView.addObject("product", product);
-        /*
+
         //seller
         Optional<Member> optionalSeller = memberRepository.findByNickname(product.getMember().getNickname());
         if(optionalSeller.isEmpty()){
@@ -61,7 +62,7 @@ public class ChatController {
         }
         Member seller = optionalSeller.get();
         modelAndView.addObject("seller", seller);
-        */
+
 
         //sender
         Optional<Member> optionalMember = memberRepository.findByNickname(nickname);
@@ -73,38 +74,39 @@ public class ChatController {
         //chatRoom
         Optional<ChatRoom> optionalChatRoom = chatService.getChatRoomByProductIdAndNickname(product.getId(), sender.getNickname());
         if(optionalChatRoom.isEmpty()){
-            throw new NotFoundException("채팅방이 없습니다.");
-            //return createChatRoom(product, sender,"product", productId);    //rollback 필요
+            //throw new NotFoundException("채팅방이 없습니다.");
+            return createChatRoom(product, seller, sender,"product", productId);    //rollback 필요
         }
         modelAndView.addObject("chatRoom", optionalChatRoom.get());
         //chatMessages
         List<ChatMessage> chatMessages = chatService.getChatMessagesByChatRoomId(optionalChatRoom.get().getId());
         modelAndView.addObject("chatMessages", chatMessages);
 
+        //modelAndView.setViewName("chat/chatRoom");
         modelAndView.setViewName("chat/chatRoom");
-        //modelAndView.setViewName("thyme/chat/chatRoom");
 
         return modelAndView;
     }
-    /*
+
     //입장 - afterService
     @GetMapping("/chatRoom/afterService/{afterServiceId}")
     public ModelAndView getChatRoomByAfterServiceId(@PathVariable long afterServiceId, HttpSession session) {
         ModelAndView modelAndView = new ModelAndView();
-        //afterService, asSeller, sender, chatRoom, chatRoomMessages
+        //afterService, seller, sender, chatRoom, chatRoomMessages
         String nickname = (String) session.getAttribute("loggedInMember");
         //afterService
-        AfterService afterService = asRepository.findById(afterServiceId);
-        modelAndView.addObject("afterService", afterService);
-        //////////////////////////////////////////////////
-        //asSeller
-        Optional<Member> optionalSeller = memberRepository.findByNickname(product.getMember().getNickname());
+        Optional<AfterService> optionalAfterService = asRepository.findById(afterServiceId);
+        if(optionalAfterService.isEmpty()){
+            throw new NotFoundException("afterService가 없습니다.");
+        }
+        modelAndView.addObject("afterService", optionalAfterService.get());
+        //seller
+        Optional<Member> optionalSeller = memberRepository.findByNickname(optionalAfterService.get().getMember().getNickname());
         if(optionalSeller.isEmpty()){
             throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
         }
         Member seller = optionalSeller.get();
         modelAndView.addObject("seller", seller);
-        /////////////////////////////////////////////////
 
         //sender
         Optional<Member> optionalMember = memberRepository.findByNickname(nickname);
@@ -114,10 +116,9 @@ public class ChatController {
         Member sender = optionalMember.get();
         modelAndView.addObject("sender", sender);
         //chatRoom
-        Optional<ChatRoom> optionalChatRoom = chatService.getChatRoomByAfterServiceIdAndNickname(afterServiceId, nickname);
+        Optional<ChatRoom> optionalChatRoom = chatService.getChatRoomByAfterServiceIdAndNickname(optionalAfterService.get().getId(), sender.getNickname());
         if(optionalChatRoom.isEmpty())
-            throw new NotFoundException("채팅방이 없습니다.");
-            //return createChatRoom(afterService, sender,"afterService", afterServiceId);    //생성
+            return createASChatRoom(optionalAfterService.get(), seller, sender,"afterService", afterServiceId);    //생성
         modelAndView.addObject("chatRoom", optionalChatRoom.get());
 
         //chatRoomMessages
@@ -130,18 +131,36 @@ public class ChatController {
 
         return modelAndView;
     }
-    */
     //생성
     @PostMapping("/createChatRoom")
-    private ModelAndView createChatRoom(Product product, Member sender, String type, long id) {
+    private ModelAndView createChatRoom(Product product, Member seller, Member sender, String type, long id) {
         ModelAndView modelAndView = new ModelAndView();
         ChatRoom chatRoom = chatService.insertChatRoom(type, id);
-
+        List<ChatMessage> chatRoomMessages = new ArrayList<>();
+        //product, seller, sender, chatRoom, chatRoomMessages
         modelAndView.addObject("chatRoom", chatRoom);
         modelAndView.addObject("product", product);
         modelAndView.addObject("sender", sender);
+        modelAndView.addObject("seller", seller);
+        modelAndView.addObject("chatRoomMessages", chatRoomMessages);
         modelAndView.addObject("message", "Chat room created successfully");
-        modelAndView.setViewName("chat/chatRoom/"+chatRoom.getId());
+        modelAndView.setViewName("chat/chatRoom");
+
+        return modelAndView;
+    }
+    @PostMapping("/createASChatRoom")
+    private ModelAndView createASChatRoom(AfterService as, Member seller, Member sender, String type, long id) {
+        ModelAndView modelAndView = new ModelAndView();
+        ChatRoom chatRoom = chatService.insertChatRoom(type, id);
+        List<ChatMessage> chatRoomMessages = new ArrayList<>();
+        //product, seller, sender, chatRoom, chatRoomMessages
+        modelAndView.addObject("chatRoom", chatRoom);
+        modelAndView.addObject("product", as);
+        modelAndView.addObject("sender", sender);
+        modelAndView.addObject("seller", seller);
+        modelAndView.addObject("chatRoomMessages", chatRoomMessages);
+        modelAndView.addObject("message", "Chat room created successfully");
+        modelAndView.setViewName("chat/chatRoom");
 
         return modelAndView;
     }
@@ -158,7 +177,7 @@ public class ChatController {
     }
 
     @MessageMapping("/{roomId}") //여기로 전송되면 메서드 호출 -> WebSocketConfig prefixes 에서 적용한건 앞에 생략
-    @SendTo("/room/{roomId}")   //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야됨
+    @SendTo("/room/{roomId}")   //구독하고 있는 장소로 메시지 전송 (목적지)  -> WebSocketConfig Broker 에서 적용한건 앞에 붙어줘야 함
     public ChatMessageDto sendMessage(@DestinationVariable Long roomId, ChatMessageDto message) {
         ChatMessage mm = chatService.createMessage(roomId, message.getMember(), message.getMessage());
         return ChatMessageDto.builder()
